8b0c58ff3262591546f11073426af0267e6b7d7f
LeoGalisse <leonardogalisse@gmail.com>
2023-04-28 03:05:17 -0300

    feat: add tokens and documentation for tokens
    add tokens for colors, font-sizes, font-weights, fonts, line-height, radd, space and docs in storybook

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  white: '#FFFFFF',
+
+  blue100: '#DBEAFE',
+  blue200: '#BFDBFE',
+  blue400: '#60A5FA',
+  blue600: '#2563EB',
+  blue800: '#1E40AF',
+  blue900: '#1E3A8A',
+  blue950: '#172554',
+
+  stone600: '#57534E',
+  stone950: '#0C0A09',
+}
---
+++ packages/tokens/src/fonts.ts
@@ -0,0 +1,4 @@
+export const fonts = {
+  default: 'Open sans, sans-serif',
+  code: 'monospace',
+}
